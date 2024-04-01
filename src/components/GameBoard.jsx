5a04839cef3ac83da7ae855f6b12737ee44b0a2e
Malav S Patel <malavpatel2607775@gmail.com>
2024-03-31 22:25:45 -0400

    Added Eventhandler and useStates

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,46 @@
-function Square() {
+import { useState } from "react";
+
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked");
+  }
   return (
     <>
-      <button>testbutton</button>
+      <button onClick={handleClick}>{value}</button>
     </>
   );
 }
 
 export default function GameBoard() {
+  const [nextIs, setNextIs] = useState("X");
+  const [squares, setSquares] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
   return (
     <>
-      <Square />
+      <div>
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
+      <div>
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
+      <div>
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
     </>
   );
 }
